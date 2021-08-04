53536996036b97836219d02b2d2c635846c13be3
Shubh AAgrawal <shubh@caltech.edu>
2021-08-04 13:39:47 -0700

    add plotting to telluric

--- get_telluric_calibration.py
+++ get_telluric_calibration.py
@@ -3,6 +3,7 @@ sys.path.append("/scr3/jruffio/shubh/breads/")
 import breads.calibration as cal
 import breads.instruments.OSIRIS as o
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 try:
     import mkl
@@ -10,12 +11,12 @@ try:
 except:
     pass
 
-dir_name = "/scr3/jruffio/data/osiris_survey/targets/SR3/210628/second/reduced/"
-# dir_name = "/scr3/jruffio/data/osiris_survey/targets/HIP73049/210627/reduced/"
+# dir_name = "/scr3/jruffio/data/osiris_survey/targets/SR3/210628/second/reduced/"
+dir_name = "/scr3/jruffio/data/osiris_survey/targets/HIP73049/210628/reduced/"
 files = os.listdir(dir_name)
 
 print("making subdirectories")
-Path(dir_name+"spectra/").mkdir(parents=True, exist_ok=True)
+Path(dir_name+"spectra/plots").mkdir(parents=True, exist_ok=True)
 
 wvs_phoenix = "/scr3/jruffio/data/kpic/models/phoenix/WAVE_PHOENIX-ACES-AGSS-COND-2011.fits"
 A0_phoenix = "/scr3/jruffio/models/phoenix/fitting/phoenix.astro.physik.uni-goettingen.de/HiResFITS/PHOENIX-ACES-AGSS-COND-2011/Z-0.0/lte09000-4.00-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits"
@@ -27,4 +28,13 @@ for fil in files:
     print(fil)
     data = o.OSIRIS(dir_name+fil)
     _ = data.remove_bad_pixels(med_spec="transmission")
-    _ = cal.telluric_calibration(data, star_spectrum, mask=True, verbose=True, calib_filename=dir_name+"spectra/"+fil[:-5]+"_spectrum.fits")
+    obj = cal.telluric_calibration(data, star_spectrum, mask=True, verbose=True, calib_filename=dir_name+"spectra/"+fil[:-5]+"_spectrum.fits")
+    print("plotting")
+    plt.figure()
+    plt.plot(obj.read_wavelengths, obj.transmission)
+    plt.savefig(dir_name+"spectra/plots/"+fil[:-5]+"_transmission.png")
+    plt.close()
+    plt.figure()
+    plt.plot(obj.read_wavelengths, obj.fluxes)
+    plt.savefig(dir_name+"spectra/plots/"+fil[:-5]+"_flux.png")
+    plt.close()
