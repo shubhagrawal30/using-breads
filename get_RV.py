5c9e62825ec30dfd4cf00c69b5e079921a393d68
Shubh AAgrawal <shubh@caltech.edu>
2021-08-05 19:45:28 -0700

    add res pos to RV

--- get_RV.py
+++ get_RV.py
@@ -21,7 +21,8 @@ from breads.search_planet import search_planet
 from breads.fm.hc_splinefm import hc_splinefm
 
 numthreads = 4
-dir_name = "/scr3/jruffio/data/osiris_survey/targets/HD148352/210626/reduced/"
+# dir_name = "/scr3/jruffio/data/osiris_survey/targets/HD148352/210626/reduced/"
+dir_name = "/scr3/jruffio/data/osiris_survey/targets/ROXs4/210627/reduced/"
 
 print("making subdirectories")
 Path(dir_name+"planets/").mkdir(parents=True, exist_ok=True)
@@ -33,23 +34,34 @@ arr = np.genfromtxt(planet_btsettl, delimiter=[12, 14], dtype=np.float64,
 model_wvs = arr[:, 0] / 1e4
 model_spec = 10 ** (arr[:, 1] - 8)
 
-filename = "s210626_a032002_Kn5_020.fits"
+# filename = "s210626_a032002_Kn5_020.fits"
+filename = "s210627_a046004_Kn5_020.fits"
 print(filename)
 dataobj = OSIRIS(dir_name+filename) 
 nz,ny,nx = dataobj.data.shape
 dataobj.noise = np.ones((nz,ny,nx))
 
-sky_calib_file = "/scr3/jruffio/data/osiris_survey/targets/calibration_skys/210626/reduced/s210626_a003002_Kn3_020_calib.fits"
+# sky_calib_file = "/scr3/jruffio/data/osiris_survey/targets/calibration_skys/210626/reduced/s210626_a003002_Kn3_020_calib.fits"
+sky_calib_file = "/scr3/jruffio/data/osiris_survey/targets/calibration_skys/210627/reduced/s210627_a003002_Kn3_020_calib.fits"
 dataobj.calibrate(sky_calib_file)
 
 spec_file = dir_name+"spectra/"+filename[:-5]+"_spectrum.fits"
 print("Reading spectrum file", spec_file)
 with pyfits.open(spec_file) as hdulist:
     star_spectrum = hdulist[2].data
+    mu_x = hdulist[3].data
+    mu_y = hdulist[4].data
+
+print("setting reference position")
+dataobj.set_reference_position((np.nanmedian(mu_y), np.nanmedian(mu_x)))
+# dataobj.set_reference_position((2, 2))
+print(dataobj.refpos)
 
 # tr_file = dir_name+"spectra/"+filename[:-5]+"_spectrum.fits"
 # SR3
-tr_file = "/scr3/jruffio/data/osiris_survey/targets/SR3/210626/first/reduced/spectra/s210626_a025"+filename[12:-13]+"_Kn5_020_spectrum.fits"
+# tr_file = "/scr3/jruffio/data/osiris_survey/targets/SR3/210626/first/reduced/spectra/s210626_a025"+filename[12:-13]+"_Kn5_020_spectrum.fits"
+tr_file = "/scr3/jruffio/data/osiris_survey/targets/SR3/210627/second/reduced/spectra/s210627_a053002_Kn5_020_spectrum.fits"
+
 print("Reading transmission file", tr_file)
 with pyfits.open(tr_file) as hdulist:
     transmission = hdulist[0].data
@@ -66,16 +78,18 @@ model_broadspec = dataobj.broaden(model_wvs,model_spec)
 planet_f = interp1d(model_wvs, model_broadspec, bounds_error=False, fill_value=np.nan)
 
 fm_paras = {"planet_f":planet_f,"transmission":transmission,"star_spectrum":star_spectrum,
-        "boxw":3,"nodes":20,"psfw":1.2,"nodes":20,"badpixfraction":0.75}
+        "boxw":3,"nodes":5,"psfw":1.5,"badpixfraction":0.75}
 fm_func = hc_splinefm
 # from breads.fm.hc_hpffm import hc_hpffm
 # fm_paras = {"planet_f":planet_f,"transmission":transmission,"star_spectrum":star_spectrum,
 #             "boxw":1,"psfw":1.2,"badpixfraction":0.75,"hpf_mode":"fft","cutoff":40}
 # fm_func = hc_hpffm
-rvs = np.linspace(-2000,2000,2001)
-# rvs = [-7]
-ys = [43]
-xs = [44]
+# rvs = np.linspace(-2000,2000,2001)
+rvs = [-7]
+# ys = [43]
+# xs = [44]
+ys = [-2, -1, 0]
+xs = [-13, -14, -15]
 
 if False: # Example code to test the forward model
     nonlin_paras = [0,34,39] # rv (km/s), y (pix), x (pix)
@@ -121,15 +135,15 @@ print("plotting")
 # plt.title(rvs[np.nanargmax(np.exp(out[:,0,0,0]-np.max(out[:,0,0,0])))])
 # plt.show()
 
-plt.figure()
-plt.plot(rvs,out[:,0,0,3]/out[:,0,0,3+N_linpara])
-plt.ylabel("SNR")
-plt.xlabel("RV (km/s)")
-plt.show()
-
 # plt.figure()
-# plt.imshow(out[0,:,:,3]/out[0,:,:,3+N_linpara],origin="lower")
-# cbar = plt.colorbar()
-# cbar.set_label("SNR")
+# plt.plot(rvs,out[:,0,0,3]/out[:,0,0,3+N_linpara])
+# plt.ylabel("SNR")
+# plt.xlabel("RV (km/s)")
 # plt.show()
-# plt.close()
+
+plt.figure()
+plt.imshow(out[0,:,:,3]/out[0,:,:,3+N_linpara],origin="lower")
+cbar = plt.colorbar()
+cbar.set_label("SNR")
+plt.show()
+plt.close()
