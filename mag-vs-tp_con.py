2b50b946692db261146af17ddf924cb7b22f2500
Shubh AAgrawal <shubh@caltech.edu>
2021-08-30 16:42:42 -0700

    start nodes-vs

--- mag-vs-tp_con.py
+++ mag-vs-tp_con.py
@@ -45,7 +45,11 @@ def line_from_scatter(xvals, yvals, num, equal_bins=False, log_space=True):
 targets = ["HD148352", "SR3", "SR14", "ROXs44", "ROXs43B", "SR9", "SR4", "ROXs8", "ROXs35A", "SR21A"]
 Kmags = [6.511, 6.504, 8.878, 7.61, 7.09, 7.207, 7.518, 6.227, 8.531, 6.719]
 spec_types = ["F2V", "A0", "G5", "K3e", "K3e", "K5e", "K0:Ve", "K0", "K1IV", "G1"]
+Teffs = [6700, 4000, 5400, 4700, 4000, 3900, 4300, 5200, 4700, 4500]
 threshold_snr = 5
+
+# tp60, tp100_err, con60, con60_err = [], [], [], []
+tp100, tp100_err, con100, con100_err = [], [], [], []
 
 for target, Kmag, spec_type in zip(targets, Kmags, spec_types):
     print(target)
@@ -66,27 +70,58 @@ for target, Kmag, spec_type in zip(targets, Kmags, spec_types):
     plt.figure(1)
     plt.scatter(distances, noise, label=label, alpha=0.1)
     plt.figure(2)
-    plt.scatter(distances, tp, label=label, alpha=0.1)
-    plt.figure(3)
     rxs, rys, xerrs, yerrs = line_from_scatter(distances, noise, 100, False)
     plt.errorbar(rxs, rys, yerr=yerrs, xerr=xerrs, label=label, alpha=0.6, ls='none')
-    plt.figure(4)
+    plt.figure(3)
     rxs, rys, xerrs, yerrs = line_from_scatter(distances, noise, 100, True)
     plt.errorbar(rxs, rys, yerr=yerrs, xerr=xerrs, label=label, alpha=0.6, ls='dotted')
+    con100 += [rys[rxs.index(100.)]]
+    con100_err += [yerrs[rxs.index(100.)]]
 
-for ind in [1, 2, 3, 4]:
+    plt.figure(4)
+    plt.scatter(distances, tp, label=label, alpha=0.1)
+    plt.figure(5)
+    rxs, rys, xerrs, yerrs = line_from_scatter(distances, tp, 100, False)
+    plt.errorbar(rxs, rys, yerr=yerrs, xerr=xerrs, label=label, alpha=0.6, ls='none')
+    plt.figure(6)
+    rxs, rys, xerrs, yerrs = line_from_scatter(distances, tp, 100, True)
+    plt.errorbar(rxs, rys, yerr=yerrs, xerr=xerrs, label=label, alpha=0.6, ls='dotted')
+    tp100 += [rys[rxs.index(100.)]]
+    tp100_err += [yerrs[rxs.index(100.)]]
+
+plt.figure(7)
+plt.errorbar(Kmags, tp100, yerr=tp100_err, label="100 mas", ls='none')
+plt.figure(8)
+plt.errorbar(Kmags, con100, yerr=con100_err, label="100 mas", ls='none')
+
+plt.figure(9)
+plt.errorbar(Teffs, tp100, yerr=tp100_err, label="100 mas", ls='none')
+plt.figure(10)
+plt.errorbar(Teffs, con100, yerr=con100_err, label="100 mas", ls='none')
+
+for ind in range(1, 11):
     print("plotting", ind)
     plt.figure(ind)
-    plt.legend(loc='upper center', bbox_to_anchor=(-0.14,-0.32,1.16,0.2), ncol=5, mode='expand', fontsize='x-small')
-    plt.xlabel("separation in mas")
-    if ind == 2:
+    if ind <= 6:
+        plt.legend(loc='upper center', bbox_to_anchor=(-0.14,-0.33,1.16,0.2), ncol=5, mode='expand', fontsize='x-small')
+        plt.xlabel("separation in mas")
+    elif ind <= 8:
+        plt.legend()
+        plt.xlabel("K magnitude")
+    elif ind <= 10:
+        plt.legend()
+        plt.xlabel("effective temperature")
+    if 4 <= ind <= 7 or ind == 9:
         plt.ylabel("recovered / injected flux")
         plt.title("throughputs")
-    else:
+    elif ind <= 4:
         plt.xscale('log')
         plt.yscale('log')
         plt.ylabel("flux ratio")
         plt.ylim([1e-4, 1e-2])
+        plt.title(f"contrast curves (SNR {threshold_snr})")
+    elif ind == 8 or ind == 10:
+        plt.ylabel("flux ratio")
         plt.title(f"contrast curves (SNR {threshold_snr})")
     plt.grid()
     plt.tight_layout()
---
+++ nodes-vs-tp_con.py
@@ -0,0 +1,173 @@
+print("starting")
+import sys
+from ipywidgets.widgets.widget_output import Output
+sys.path.append("/scr3/jruffio/shubh/breads/")
+import numpy as np
+import matplotlib.pyplot as plt
+from  scipy.interpolate import interp1d
+import os
+import astropy.io.fits as pyfits
+from pathlib import Path
+from breads.fit import fitfm
+from copy import deepcopy
+import multiprocessing as mp
+from itertools import repeat
+from multiprocessing.pool import ThreadPool
+from concurrent.futures import ProcessPoolExecutor as Pool
+
+numthreads = 16
+
+print("Importing mkl")
+try:
+    import mkl
+    mkl.set_num_threads(1)
+except:
+    pass
+
+print("Importing breads")
+from breads.instruments.OSIRIS import OSIRIS
+from breads.search_planet import search_planet
+from breads.fm.hc_splinefm import hc_splinefm
+from breads.fm.hc_no_splinefm import hc_no_splinefm
+from breads.fm.hc_hpffm import hc_hpffm
+from breads.injection import inject_planet, read_planet_info
+
+dir_name = "/scr3/jruffio/data/osiris_survey/targets/SR4/210627/reduced/"
+files = os.listdir(dir_name)
+
+subdirectory = "throughput/TP/"
+
+print("making subdirectories")
+Path(dir_name+subdirectory+"plots/").mkdir(parents=True, exist_ok=True)
+
+print("Reading planet file")
+planet_btsettl = "/scr3/jruffio/models/BT-Settl/BT-Settl_M-0.0_a+0.0/lte018-5.0-0.0a+0.0.BT-Settl.spec.7"
+arr = np.genfromtxt(planet_btsettl, delimiter=[12, 14], dtype=np.float64,
+                converters={1: lambda x: float(x.decode("utf-8").replace('D', 'e'))})
+model_wvs = arr[:, 0] / 1e4
+model_spec = 10 ** (arr[:, 1] - 8)
+
+tr_dir = "/scr3/jruffio/data/osiris_survey/targets/SR3/210627/first/reduced/spectra/"
+tr_files = os.listdir(tr_dir)
+if "plots" in tr_files:
+    tr_files.remove("plots")
+tr_counter = 0
+tr_total = len(tr_files)
+
+sky_calib_file = "/scr3/jruffio/data/osiris_survey/targets/calibration_skys/210627/reduced/s210627_a003002_Kn3_020_calib.fits"
+
+def one_location(args):
+    dataobj, location, planet_f, spec_file, transmission, flux_ratio, dat, filename = args
+    try:
+        dataobj.data = deepcopy(dat)
+        inject_planet(dataobj, location, planet_f, spec_file, transmission, flux_ratio)
+        print("SNR time", location)
+        out = search_planet([rvs,[location[0]],[location[1]]],dataobj,fm_func,fm_paras,numthreads=numthreads)
+        N_linpara = (out.shape[-1]-2)//2
+        return out[0,0,0,3], out[0,0,0,3+N_linpara]
+    except Exception as e:
+        print(e)
+        print("FAILED", filename, location)
+        return np.nan, np.nan
+
+sep = 100
+num_angles = 8
+angles = np.linspace(0, 2*np.pi, num_angles+1)[:-1]
+num_nodes = np.arange(2, 41, 2)
+rvs = np.array([0])
+ys = sep / 20 * np.cos(angles)
+xs = sep / 20 * np.sin(angles)
+for num_node in num_nodes:
+    for filename in files:
+        if ".fits" not in filename:
+            print("SKIP", filename)
+            continue
+        print("START", filename)
+        flux = np.zeros(num_angles) * np.nan
+        noise = np.zeros(num_angles) * np.nan
+
+        dataobj = OSIRIS(dir_name+filename) 
+        nz,ny,nx = dataobj.data.shape
+        print("sky calibrating")
+        dataobj.calibrate(sky_calib_file)
+
+        spec_file = dir_name+"spectra/"+filename[:-5]+"_spectrum.fits"
+        print("Reading spectrum file", spec_file)
+        with pyfits.open(spec_file) as hdulist:
+            star_spectrum = hdulist[2].data
+            mu_x = hdulist[3].data
+            mu_y = hdulist[4].data
+
+        print("setting reference position")
+        dataobj.set_reference_position((np.nanmedian(mu_y), np.nanmedian(mu_x)))
+        print(dataobj.refpos)
+
+        tr_counter = (tr_counter + 1) % tr_total
+        tr_file = tr_dir + tr_files[tr_counter]
+        print("Reading transmission file", tr_file)
+        with pyfits.open(tr_file) as hdulist:
+            transmission = hdulist[0].data
+
+        print("Removing bad pixels")
+        dataobj.remove_bad_pixels(med_spec=star_spectrum)
+        dat = deepcopy(dataobj.data)
+
+        print("setting planet model")
+        minwv,maxwv= np.nanmin(dataobj.wavelengths),np.nanmax(dataobj.wavelengths)
+        crop_btsettl = np.where((model_wvs > minwv - 0.2) * (model_wvs < maxwv + 0.2))
+        model_wvs = model_wvs[crop_btsettl]
+        model_spec = model_spec[crop_btsettl]
+        model_broadspec = dataobj.broaden(model_wvs,model_spec)
+        planet_f = interp1d(model_wvs, model_broadspec, bounds_error=False, fill_value=np.nan)
+
+        fm_paras = {"planet_f":planet_f,"transmission":transmission,"star_spectrum":star_spectrum,
+                "boxw":3,"nodes":num_node,"psfw":(np.nanmedian(mu_y), np.nanmedian(mu_x)),
+                "badpixfraction":0.75,"optimize_nodes":True}
+        fm_func = hc_no_splinefm
+        flux_ratio = 1e-2
+        
+        print("setting noise")
+        dataobj.set_noise()
+        
+        args = zip(repeat(dataobj), list(zip(ys, xs)), repeat(planet_f), repeat(spec_file),\
+            repeat(transmission), repeat(flux_ratio), repeat(dat), repeat(filename))
+
+        with Pool() as tpool:
+            for i, f, n in enumerate(tpool.map(one_location, args)):
+                print(f, n)
+                flux[i], noise[i] = f, n
+        print(flux, noise)
+        exit()
+
+        # plt.figure()
+        # plt.imshow(flux/noise,origin="lower")
+        # cbar = plt.colorbar()
+        # cbar.set_label("SNR")
+        # plt.savefig(dir_name+subdirectory+"plots/"+filename[:-5]+"_snr.png")
+        # plt.figure()
+        # plt.imshow(flux,origin="lower")
+        # cbar = plt.colorbar()
+        # cbar.set_label("flux")
+        # plt.savefig(dir_name+subdirectory+"plots/"+filename[:-5]+"_flux.png")
+        # plt.figure()
+        # plt.imshow(noise,origin="lower")
+        # cbar = plt.colorbar()
+        # cbar.set_label("noise")
+        # plt.savefig(dir_name+subdirectory+"plots/"+filename[:-5]+"_noise.png")
+        # plt.close('all')
+
+        # hdulist = pyfits.HDUList()
+        # hdulist.append(pyfits.PrimaryHDU(data=flux,
+        #     header=pyfits.Header(cards={"TYPE": "flux", "FILE": filename, "PLANET": planet_btsettl,\
+        #                                     "FLUX": spec_file, "TRANS": tr_file})))    
+        # hdulist.append(pyfits.PrimaryHDU(data=noise,
+        #     header=pyfits.Header(cards={"TYPE": "noise", "FILE": filename, "PLANET": planet_btsettl,\
+        #                                     "FLUX": spec_file, "TRANS": tr_file})))   
+                                    
+        # try:
+        #     hdulist.writeto(dir_name+subdirectory+filename[:-5]+"_out.fits", overwrite=True)
+        # except TypeError:
+        #     hdulist.writeto(dir_name+subdirectory+filename[:-5]+"_out.fits", clobber=True)
+        # hdulist.close()
+
+        # print("DONE", filename)
